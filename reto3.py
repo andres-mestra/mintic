62c1fddefbf0908ec0e3a3f121df09f13003832e
andresdev <andresmestra11a@gmail.com>
2021-06-20 11:37:08 -0500

    Cogido reto 4

--- reto3.py
+++ reto3.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 
 
 def clear():
@@ -78,25 +79,27 @@ def change_password():
     return True
 
 
+def valid_latitud(latitud):
+    if(latitud < 9.757 or latitud > 10.465):
+        print('Error coordenada')
+        return False
+    return True
+
+
+def valid_longitud(longitud):
+    if(longitud < -73.623 or longitud > -72.987):
+        print('Error coordenada')
+        return False
+    return True
+
+
+def print_coordenadas(coordenadas):
+    for i in range(len(coordenadas)):
+        print(f'coordenada [latitud,longitud] {i+1} : {coordenadas[i]}')
+
+
 def input_coordenadas():
     global coordenadas
-
-    def valid_latitud(latitud):
-        if(latitud < 9.757 or latitud > 10.465):
-            print('Error coordenada')
-            return False
-        return True
-
-    def valid_longitud(longitud):
-        if(longitud < -73.623 or longitud > -72.987):
-            print('Error coordenada')
-            return False
-        return True
-
-    def print_coordenadas(coordenadas):
-        for i in range(len(coordenadas)):
-            print(f'coordenada [latitud,longitud] {i+1} : {coordenadas[i]}')
-
     # rangos validos
     # latitud: xE{x >= 9.757,x  <= 10.462}
     # longitud: xE{x >= -73.623,x  <= -72.987}
@@ -133,8 +136,8 @@ def input_coordenadas():
             return True
     else:
         print_coordenadas(coordenadas)
-        longitudes = [coordenadas[i][-1] for i in [0, 1, 2]]
-        latitudes = [coordenadas[i][0] for i in [0, 1, 2]]
+        longitudes = [coordenadas[i][-1] for i in range(3)]
+        latitudes = [coordenadas[i][0] for i in range(3)]
         index_occidental = longitudes.index(max(longitudes))
         print(
             f'la coordenada {index_occidental+1} es la que esta más al occidente')
@@ -154,14 +157,119 @@ def input_coordenadas():
     return False
 
 
+def zonas_wifi():
+    global coordenadas
+    global zonas
+
+    def distancia(p1, p2):
+        #p1 =  [latitud, longitud]
+        # Radio de la Tierra en Km
+        R = 6372.95477598
+        delta_lat, delta_long = p2[0] - p1[0], p2[1] - p1[1]
+
+        dist = 2 * R * math.asin(math.sqrt(
+            (math.pow(math.sin(delta_lat/2), 2) +
+             math.cos(p1[0])*math.cos(p2[0])*math.pow(math.sin(delta_long/2), 2))
+        ))
+        return dist
+
+    def orientacion(origen, destino):
+        #origen = [latitud, longitud]
+        sentidos = ['', '']
+
+        # Latitud
+        if(origen[0] == destino[0]):
+            sentidos[0] = 'latitud actual'
+        elif(origen[0] < destino[0]):
+            sentidos[0] = 'norte'
+        else:
+            sentidos[0] = 'sur'
+
+        # Longitud
+        if(origen[1] == destino[1]):
+            sentidos[1] = 'longitud actual'
+        elif(origen[1] < destino[1]):
+            sentidos[1] = 'oriente'
+        else:
+            sentidos[1] = 'occidente'
+
+        return sentidos
+
+    def tiempo_promedio(distancia, medio):
+        # vmedios en m/s
+        vel_promedio_medios = {
+            'pie': 0.483,
+            'bici': 3.33 - 19.44,
+            'moto': 19.44,
+            'bus': 16.67 - 0.483,
+            'auto': 20.83
+        }
+        return distancia/vel_promedio_medios[medio]
+
+    if(coordenadas[0][0] == 0):
+        print('Error sin registro de coordenadas')
+        return False
+
+    print_coordenadas(coordenadas)
+    ubic = int(input(
+        'Por favor elija su ubicación actual (1,2 ó 3) para calcular la distancia a los puntos de conexión\n'))
+    if(ubic < 1 or ubic > 3):
+        print('Error ubicación')
+        return False
+
+    ubicacion_actual = coordenadas[ubic-1]
+    iterable_zonas = range(len(zonas))
+    ubicacion_zonas = [zonas[i][:-1] for i in iterable_zonas]
+    usuarios_zonas = [zonas[i][-1] for i in iterable_zonas]
+    distancias_puntos = [
+        (i, distancia(ubicacion_actual, ubicacion_zonas[i]), usuarios_zonas[i]) for i in iterable_zonas
+    ]
+    distancias_orden = sorted(
+        distancias_puntos, key=lambda dist: dist[1]
+    )
+    distancias_orden = sorted(
+        distancias_orden, key=lambda user: user[2]
+    )
+    print('Zonas wifi cercanas con menos usuarios')
+    for i in range(2):
+        zona = distancias_orden[i]
+        coordenada = zonas[zona[0]][:-1]
+        print(
+            f'La zona wifi {zona[0] + 1}: ubicada en {coordenada} a {zona[1]} metros , tiene en promedio {zona[2]} usuarios'
+        )
+    w1 = distancias_orden[0][0] + 1
+    w2 = distancias_orden[1][0] + 1
+    indi = int(
+        input(f'Elija {w1} o {w2} para recibir indicaciones de llegada\n')
+    )
+    if(indi == w1 or indi == w2):
+        y, x = orientacion(ubicacion_actual, zonas[indi-1])
+        print(
+            f'Para llegar a la zona wifi dirigirse primero al {x} y luego hacia el {y}')
+        # Distancia en la lista distancias_orden esta en kilometros, la función tiempo promedio en metros
+        print(
+            f'Tiempo en auto {tiempo_promedio((distancias_orden[indi][1]), "auto")}s')
+        print(
+            f'Tiempo en bicicleta {tiempo_promedio((distancias_orden[indi][1]), "bici")}s')
+        salir = int(input('Presione 0 para salir\n'))
+        if(salir == 0):
+            return True
+        else:
+            print('Error')
+            return False
+
+    print('Error zona wifi')
+    return False
+
+
 def menu(opciones, opc):
-    if(opc >= 1 and opc <= 2):
+    if(opc >= 1 and opc <= 3):
         response = opciones[opc-1][1]()
         if(response):
             return [True, opciones]
         else:
             return [False, []]
-    elif(opc >= 3 and opc <= 5):
+    elif(opc >= 4 and opc <= 5):
         print(f'Usted ha elegido la opción {opc}')
         return[False, []]
     elif(opc == 6):
@@ -222,15 +330,28 @@ def sesion():
 default = "51659"
 defaultInv = "95615"
 # test coord [[9.758, -73.622], [9.759, -73.621], [9.8, -73.62]]
-coordenadas = [[0, 0], [0, 0], [0, 0]]
-rootOpciones = [['Cambiar contraseña', change_password], ['Ingresar coordenadas actuales', input_coordenadas], ['Ubicar zona wifi más cercana'], ['Guardar archivo con ubicacción cercana'],
-                ['Actualizar registros de zonas wifi desde archivo'], ['Elegir opción de menú favorita', favorito], ['Cerrar sesión']]
+#coordenadas = [[0, 0], [0, 0], [0, 0]]
+coordenadas = [[9.758, -73.622], [9.759, -73.621], [9.8, -73.62]]
+zonas = [
+    [10.348, -73.051, 0],
+    [10.171, -73.136, 0],
+    [10.259, -73.069, 67],
+    [10.350, -73.043, 45]
+]
+rootOpciones = [
+    ['Cambiar contraseña', change_password],
+    ['Ingresar coordenadas actuales', input_coordenadas],
+    ['Ubicar zona wifi más cercana', zonas_wifi],
+    ['Guardar archivo con ubicacción cercana'],
+    ['Actualizar registros de zonas wifi desde archivo'],
+    ['Elegir opción de menú favorita', favorito], ['Cerrar sesión']
+]
 
 
 def main():
     sesion()
-    #print("Clear main")
-    clear()
+    print("Clear main")
+    # clear()
 
 
 if __name__ == "__main__":
